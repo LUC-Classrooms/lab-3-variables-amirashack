fbbc0f2e3ec07440f1f7139435c3fe83b492f612
amirashack <156958322+amirashack@users.noreply.github.com>
2024-02-01 18:05:05 +0000

    Lab 3 Complete

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
  */
 
 //declare a global variable and assign it a number value on the next line:
-
+var x = 100;
+var d = 20;
 
 function setup() {
   createCanvas(400, 300); // creates a drawing canvas
@@ -15,6 +16,8 @@ function draw() {
   background(200); // light gray
 
   // call a simple P5 drawing function, such as ellipse() using your new variable(s) in place of arguments
-  
+  ellipse(x, 100, d, d);
+  ellipse(x, 150, d, d);
+  ellipse(x, 200, d, d);
   
 }
